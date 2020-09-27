c0890c2b75681ba96074b9abaff8657aa47dea63
Arihant Jain <jainanilkumar739@gmail.com>
2020-09-27 16:31:51 +0530

    modal created...

--- src/index.js
+++ src/index.js
@@ -1,16 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDom from "react-dom";
-import PrimaryButton, {
-  SecondaryButton,
-  TertiaryButton,
-} from "./components/Buttons";
+import { ThemeProvider } from "styled-components";
+import { SignUpModal } from "./components";
+import { GlobalStyle, darkTheme, defaultTheme } from "./utils";
 
-const App = () => (
-  <div>
-    <PrimaryButton>Hello World...</PrimaryButton>
-    <SecondaryButton>Hello World...</SecondaryButton>
-    <TertiaryButton>Hello World...</TertiaryButton>
-  </div>
-);
+const App = () => {
+  const [useDarkTheme, setUseDarkTheme] = useState(false);
+  return (
+    <ThemeProvider theme={useDarkTheme ? darkTheme : defaultTheme}>
+      <button
+        style={{ margin: "0, 16px 24px", padding: "8px", background: "none" }}
+        onClick={() => setUseDarkTheme(true)}
+      >
+        dark theme
+      </button>
+      <button
+        style={{ margin: "0, 16px 24px", padding: "8px", background: "none" }}
+        onClick={() => setUseDarkTheme(false)}
+      >
+        default theme
+      </button>
+      <div
+        style={{
+          background: useDarkTheme
+            ? defaultTheme.primaryColor
+            : darkTheme.primaryColor,
+          width: "100vw",
+          height: "100vh",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "space-around",
+        }}
+      >
+        <SignUpModal />
+      </div>
+      <GlobalStyle />
+    </ThemeProvider>
+  );
+};
 
 ReactDom.render(<App />, document.querySelector("#root"));
