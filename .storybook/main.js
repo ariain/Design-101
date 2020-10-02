0b5fb50e9f85acdaf9801fc5b5cfd87f94acd58b
Arihant Jain <jainanilkumar739@gmail.com>
2020-10-02 13:58:55 +0530

    main.js edited and build created...

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,19 +1,19 @@
 module.exports = {
-  stories: ["../src/**/*.stories.(js|mdx|jsx|ts|tsx)"],
+  stories: ["../src/**/*.stories.@(js|mdx|jsx|ts|tsx)"],
   addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/preset-create-react-app",
-    "@storybook/addon-actions",
-    "@storybook/addon-contexts/register",
-    "@storybook/addon-backgrounds/register",
-    "@storybook/addon-knobs/register",
-    "@storybook/addon-a11y",
     {
       name: "@storybook/addon-docs",
       options: {
         configureJSX: true
       }
-    }
+    },
+    "@storybook/addon-essentials",
+    "@storybook/preset-create-react-app",
+    "@storybook/addon-actions",
+    "@storybook/addon-links",
+    "@storybook/addon-contexts/register",
+    "@storybook/addon-backgrounds/register",
+    "@storybook/addon-knobs/register",
+    "@storybook/addon-a11y"
   ]
 };
---
+++ storybook-static/precache-manifest.1e42606da6a2482e30d895213d00b88e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f3c752f6ff44804138b1f06bd3a8a9e",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.be73813c6c92a91cf0ae.bundle.js"
+  },
+  {
+    "url": "runtime~main.be73813c6c92a91cf0ae.bundle.js"
+  },
+  {
+    "revision": "5c60896dad2b42783a8b5f5c8cbabb7e",
+    "url": "static/media/sign-in.5c60896d.svg"
+  },
+  {
+    "revision": "cde752c183102ba424fb8f58f648dfb7",
+    "url": "static/media/sign-up.cde752c1.svg"
+  },
+  {
+    "url": "vendors~main.be73813c6c92a91cf0ae.bundle.js"
+  },
+  {
+    "url": "vendors~main.be73813c6c92a91cf0ae.bundle.js.LICENSE.txt"
+  }
+]);
